0ad1b5f54aa0e65a9c85bc9bf10da3ede2322075
Jean-Raphaël Houdouin <raphael.houdouin@gmail.com>
2024-12-29 16:18:38 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,15 +1,3 @@
-import streamlit as st
-from joblib import load
-from db_helper import recommend, fetch_movie_poster  # Only import helper functions
-
-# Load model and data
-load_data = load("artifacts/save_model.joblib")
-dataframe = load_data['dataframe']
-similarity = load_data['similarity']
-
-# Get movie titles
-input_data = dataframe['original_title'].values
-
 # Streamlit app title and subheading - Centered with custom CSS
 st.markdown(
     """
@@ -23,7 +11,7 @@ st.markdown(
         text-align: center;
         color: lightgray;
         margin-top: -10px; /* Negative margin to bring the subheading closer */
-        margin-bottom: 20px; /* Space below the subheading */
+        margin-bottom: 30px; /* Increased space below the subheading */
     }
     .instructions {
         margin-bottom: -10px; /* Reduce space after instructions */
@@ -36,7 +24,7 @@ st.markdown(
 # Centered title
 st.markdown('<p class="title">REEL IT IN 🎬</p>', unsafe_allow_html=True)
 
-# Centered subheading for the app with light grey color and reduced space
+# Centered subheading for the app with light grey color and added space
 st.markdown('<h3 class="subheading">Movies Recommender</h3>', unsafe_allow_html=True)
 
 # Instructions for the user
@@ -48,37 +36,3 @@ st.markdown("""
 # Add space after the text using CSS styling
 st.markdown("<style>div.stSelectbox { margin-top: 20px; }</style>", unsafe_allow_html=True)
 
-# Dropdown for movie input with searchable feature
-select_input = st.selectbox(
-    "Choose a movie:",
-    [""] + input_data,
-    key="movie_select"
-)
-
-# Add a loading spinner during recommendation processing
-if st.button("Get Similar Movie Suggestions"):
-    if select_input:
-        with st.spinner('Fetching recommendations...'):
-            # Get recommendations
-            recommended_movies = recommend(select_input)
-            st.markdown("**Recommended Movies:**")
-
-            # Display movies with posters (3 per row)
-            cols = st.columns(3)  # Create 3 columns
-            for i, movie in enumerate(recommended_movies):
-                with cols[i % 3]:  # Loop through columns
-                    poster_url = fetch_movie_poster(movie)  # Fetch poster
-                    st.image(poster_url, caption=movie, use_column_width=True)
-
-                # Create new rows every 3 movies
-                if (i + 1) % 3 == 0 and i != len(recommended_movies) - 1:
-                    cols = st.columns(3)  # Create a new set of 3 columns
-    else:
-        st.warning("Please select a movie to get recommendations.")
-
-# Credits at the bottom of the main page
-st.markdown("""
-    ---
-    **Developed by [rhoudouin](https://github.com/rhoudouin).**  
-    For inquiries or feedback, feel free to visit the GitHub profile.
-""")
