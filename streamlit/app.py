98af72146b2b596cdba342103698654b3ead6a6f
Jean-Raphaël Houdouin <raphael.houdouin@gmail.com>
2024-12-29 14:46:22 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -47,7 +47,41 @@ st.markdown(
         font-size: 16px;
         margin: 4px 2px;
         cursor: pointer;
-        border-radius
+        border-radius: 8px;
+    }
+    div.stButton > button:first-child:hover {
+        background-color: #45a049;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
+# Récupération des recommandations
+if st.button("Get Similar Movie Suggestions"):
+    if select_input:
+        with st.spinner("Fetching movie recommendations..."):
+            # Appel des fonctions pour obtenir les recommandations
+            recommended_movies = recommend(select_input)
+            st.markdown("### Recommended Movies:")
 
+            # Affichage des films avec les affiches (3 par ligne)
+            cols = st.columns(3)  # Crée 3 colonnes
+            for i, movie in enumerate(recommended_movies):
+                with cols[i % 3]:  # Itère sur les colonnes
+                    poster_url = fetch_movie_poster(movie)  # Récupère l'affiche
+                    st.image(poster_url, caption=movie, use_column_width=True)
+
+                # Ajoute une nouvelle ligne toutes les 3 colonnes
+                if (i + 1) % 3 == 0 and i != len(recommended_movies) - 1:
+                    cols = st.columns(3)  # Crée un nouveau set de colonnes
+    else:
+        st.warning("Please select a movie to get recommendations.")
+
+# Crédits
+st.markdown("""
+    ---
+    **Developed by [rhoudouin](https://github.com/rhoudouin).**  
+    For inquiries or feedback, feel free to visit the GitHub profile.
+""")
 
