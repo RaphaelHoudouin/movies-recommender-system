a16287db120a416e7d79c7cc8cc2797016709a6d
Jean-Raphaël Houdouin <raphael.houdouin@gmail.com>
2024-12-29 23:15:25 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,7 +12,43 @@ similarity = load_data['similarity']
 # Get movie titles
 input_data = dataframe['original_title'].values
 
-# Streamlit app title and subheading - Centered with custom CSS
+# Function for displaying the music player
+def display_music_player():
+    music_files = [
+        'Johann Strauss II - The Blue Danube.mp3',
+        'Dmitri Shostakovich - Walzer Nr 2.mp3',
+        'Johannes Brahms - Hungarian Dance no. 5.mp3',
+        'Richard Wagner - Ride of the Valkyries.mp3'
+    ]
+    
+    selected_music = random.choice(music_files)
+    music_file = os.path.join('streamlit', 'musics', selected_music)
+
+    if os.path.exists(music_file):
+        st.write('<p style="font-size:20px; text-align:left;">🎵 Play Music</p>', unsafe_allow_html=True)
+        st.audio(music_file, start_time=0)
+    else:
+        st.error(f"File not found: {music_file}")
+
+# Function for displaying the movie recommendation
+def display_movie_recommendations(recommended_movies):
+    st.markdown("**Recommended Movies:**")
+
+    if len(recommended_movies) == 0:
+        st.warning("Sorry, no recommendations available.")
+    else:
+        # Display movies with posters (3 per row)
+        cols = st.columns(3)  # Create 3 columns
+        for i, movie in enumerate(recommended_movies):
+            with cols[i % 3]:  # Loop through columns
+                poster_url = fetch_movie_poster(movie)  # Fetch poster
+                st.image(poster_url, caption=movie, use_column_width=True)
+
+            # Create new rows every 3 movies
+            if (i + 1) % 3 == 0 and i != len(recommended_movies) - 1:
+                cols = st.columns(3)  # Create a new set of 3 columns
+
+# Custom CSS for styling the app
 st.markdown(
     """
     <style>
@@ -25,45 +61,22 @@ st.markdown(
     .subheading {
         text-align: center;
         color: lightgray;
-        margin-top: -20px; /* Negative margin to bring the subheading closer */
-        margin-bottom: 35px; /* Increased space below the subheading */
+        margin-top: -20px;
+        margin-bottom: 35px;
     }
     .instructions {
-        margin-bottom: -10px; /* Reduce space after instructions */
+        margin-bottom: -10px;
     }
     </style>
     """,
     unsafe_allow_html=True
 )
 
-# List of available music files in the 'musics' folder
-music_files = [
-    'Johann Strauss II - The Blue Danube.mp3',
-    'Dmitri Shostakovich - Walzer Nr 2.mp3',
-    'Johannes Brahms - Hungarian Dance no. 5.mp3',
-    'Richard Wagner - Ride of the Valkyries.mp3'
-]
+# Display music player
+display_music_player()
 
-# Select a random music file
-selected_music = random.choice(music_files)
-
-# Construct the relative path to the selected file
-music_file = os.path.join('streamlit', 'musics', selected_music)
-
-# Check if the file exists
-if not os.path.exists(music_file):
-    st.error(f"File not found: {music_file}")
-else:
-    # Add non-clickable information above the audio player, aligned to the left
-    st.write('<p style="font-size:20px; text-align:left;">🎵 Play Music</p>', unsafe_allow_html=True)
-
-    # Play the music automatically when the app opens, without displaying the title
-    st.audio(music_file, start_time=0)
-
-# Centered title
+# Centered title and subheading
 st.markdown('<p class="title">REEL IT IN 🎬</p>', unsafe_allow_html=True)
-
-# Centered subheading for the app with light grey color and added space
 st.markdown('<h3 class="subheading">Movies Recommender</h3>', unsafe_allow_html=True)
 
 # Instructions for the user
@@ -88,18 +101,7 @@ if st.button("Get Similar Movie Suggestions"):
         with st.spinner('Fetching recommendations...'):
             # Get recommendations
             recommended_movies = recommend(select_input)
-            st.markdown("**Recommended Movies:**")
-
-            # Display movies with posters (3 per row)
-            cols = st.columns(3)  # Create 3 columns
-            for i, movie in enumerate(recommended_movies):
-                with cols[i % 3]:  # Loop through columns
-                    poster_url = fetch_movie_poster(movie)  # Fetch poster
-                    st.image(poster_url, caption=movie, use_column_width=True)
-
-                # Create new rows every 3 movies
-                if (i + 1) % 3 == 0 and i != len(recommended_movies) - 1:
-                    cols = st.columns(3)  # Create a new set of 3 columns
+            display_movie_recommendations(recommended_movies)
     else:
         st.warning("Please select a movie to get recommendations.")
 
@@ -109,6 +111,4 @@ st.markdown("""
     **Developed by [rhoudouin](https://github.com/rhoudouin).**  
     For inquiries or feedback, feel free to visit the GitHub profile.
 """)
-
-
 
