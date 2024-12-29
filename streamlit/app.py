f54adfa1bbbb3669e69493c9ffe84eb949e6a852
Jean-Raphaël Houdouin <raphael.houdouin@gmail.com>
2024-12-29 15:09:45 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,6 +12,7 @@ input_data = dataframe['original_title'].values
 
 # Streamlit app title
 st.title('REEL IT IN 🎬')
+st.subheader('The Movie Recommendation App')
 
 # Dropdown for movie input
 select_input = st.selectbox(
