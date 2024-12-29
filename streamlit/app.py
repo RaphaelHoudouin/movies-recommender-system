fae0121be4a5c57639db8726339b858f57877126
Jean-Raphaël Houdouin <raphael.houdouin@gmail.com>
2024-12-29 14:58:28 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -15,8 +15,8 @@ st.title('REEL IT IN 🎬')
 
 # Dropdown for movie input
 select_input = st.selectbox(
-    "Select or type a movie you like, and get similar movie recommendations.
-    Enjoy discovering new films! 🍿. ",
+    """Select or type a movie you like, and get similar movie recommendations. 
+    Enjoy discovering new films! 🍿""",
     [""] + input_data
 )
 
