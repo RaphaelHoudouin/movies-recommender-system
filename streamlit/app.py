b311a9567700d2a040ab63aed2f096bc6e46b787
Jean-Raphaël Houdouin <raphael.houdouin@gmail.com>
2024-12-29 18:19:19 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -19,7 +19,7 @@ st.markdown(
         text-align: center;
         font-size: 40px;
         font-weight: bold;
-        margin-top: 40px;
+        margin-top: 35px;
     }
     .subheading {
         text-align: center;
