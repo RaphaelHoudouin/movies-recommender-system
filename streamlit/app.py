a69b26b905cdf7ded01a02fce2fb32c02ba051d0
Jean-Raphaël Houdouin <raphael.houdouin@gmail.com>
2024-12-29 15:56:51 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -22,7 +22,7 @@ st.markdown(
     .subheading {
         text-align: center;
         color: lightgray;
-        margin-top: -5px; /* Reduced space between title and subheading */
+        margin-top: -10px; /* Negative margin to bring the subheading closer */
         margin-bottom: 20px; /* Space below the subheading */
     }
     </style>
